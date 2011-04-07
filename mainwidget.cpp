4ddb4911b237db0a36208bb22cfd3ebdba97b902
anisart <anisart@anisart-laptop.(none)>
2011-04-07 17:46:49 +0400

    Добавлена сортировка строчек по времени

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -24,42 +24,43 @@ MainWidget::MainWidget(QWidget *parent) :
     QString queryString("select start_h, start_m, end_h, end_m, \
                         subject, professor, type, location \
                         from weeks where weekday = ");
+    QString querySortStr(" order by start_h, start_m");
 
     inScroll->setLayout(new QVBoxLayout);
     //inScroll->layout()->setContentsMargins(0,0,0,0);
 
     inScroll->layout()->addWidget(new QLabel("Monday"));
-    query.exec(queryString + "0");
+    query.exec(queryString + "0" + querySortStr);
     while (query.next())
         inScroll->layout()->addWidget(new Row(query,this));
 
     inScroll->layout()->addWidget(new QLabel("Thuesday"));
-    query.exec(queryString + "1");
+    query.exec(queryString + "1" + querySortStr);
     while (query.next())
         inScroll->layout()->addWidget(new Row(query,this));
 
     inScroll->layout()->addWidget(new QLabel("Wednesday"));
-    query.exec(queryString + "2");
+    query.exec(queryString + "2" + querySortStr);
     while (query.next())
         inScroll->layout()->addWidget(new Row(query,this));
 
     inScroll->layout()->addWidget(new QLabel("Thursday"));
-    query.exec(queryString + "3");
+    query.exec(queryString + "3" + querySortStr);
     while (query.next())
         inScroll->layout()->addWidget(new Row(query,this));
 
     inScroll->layout()->addWidget(new QLabel("Friday"));
-    query.exec(queryString + "4");
+    query.exec(queryString + "4" + querySortStr);
     while (query.next())
         inScroll->layout()->addWidget(new Row(query,this));
 
     inScroll->layout()->addWidget(new QLabel("Saturday"));
-    query.exec(queryString + "5");
+    query.exec(queryString + "5" + querySortStr);
     while (query.next())
         inScroll->layout()->addWidget(new Row(query,this));
 
     inScroll->layout()->addWidget(new QLabel("Sunday"));
-    query.exec(queryString + "6");
+    query.exec(queryString + "6" + querySortStr);
     while (query.next())
         inScroll->layout()->addWidget(new Row(query,this));
 
